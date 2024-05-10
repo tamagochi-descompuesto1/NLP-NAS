aac3a0af11fe0b0e651fee462a867ec81f97dffb
tamagochi-descompuesto1 <israelsanchez0109@outlook.com>
2024-05-10 11:07:36 -0600

    Add files via upload

---
+++ NLP_NAS/model_loader.py
@@ -0,0 +1,27 @@
+from transformers import AutoModel
+
+def model_loader(models: list) -> list:
+    downloaded_models = []
+    for model_path in models:
+        print('Downloading model', model_path, '...')
+        try:
+            model = AutoModel.from_pretrained(model_path)
+            downloaded_models.append(model)
+        except OSError:
+            print(model_path, 'not found.')
+
+    return downloaded_models
+
+def save_models(models: list, model_names: list) -> None:
+    for index, model in enumerate(models):
+        model.save_pretrained('models/' + model_names[index])
+    return
+
+
+if __name__ == '__main__': 
+    with open('models/models.txt', 'r') as file:
+        models = file.read().splitlines()
+    file.close()
+
+    downloaded_models = model_loader(models)
+    save_models(downloaded_models, models)
